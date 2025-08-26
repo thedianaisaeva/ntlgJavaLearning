4cbc45bf78c5b5683cdb386d1fd46592bb89e559
thedianaisaeva <thedianaisaeva@gmail.com>
2025-08-26 22:08:48 +0300

    Add files via upload
    HW_001_260825

---
+++ TaskOne.java
@@ -0,0 +1,8 @@
+class TaskOne {
+    public static void main(String[] args) {
+        System.out.println("Diana Isaeva");
+    }
+}
+
+
+
---
+++ TaskTwo.java
@@ -0,0 +1,9 @@
+class TaskTwo {
+    public static void main(String[] args) {
+        int income = 350000;
+        int spending = 126800;
+        System.out.println("Итого остаток:");
+        System.out.println(income - spending + " руб.");
+    }
+}
+
