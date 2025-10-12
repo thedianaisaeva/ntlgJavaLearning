4c3d82e9e20345e8c71c07762f7162ab8b1bc989
thedianaisaeva <thedianaisaeva@gmail.com>
2025-10-12 12:25:39 +0300

    Update MatrixRotate.java

--- HW_MASSIVE2_091025/MatrixRotate.java
+++ HW_MASSIVE2_091025/MatrixRotate.java
@@ -8,37 +8,37 @@ public class MatrixRotate {
         int[][] colors = new int[SIZE][SIZE];
         int[][] rotate = new int[SIZE][SIZE];
 
-        CreateMatrix(colors);
-        ShowMatrix(colors);
-        ShowMenu();
+        createMatrix(colors);
+        showMatrix(colors);
+        showMenu();
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
         switch (input) {
             case 1:
-                MatrixRotate90(colors, rotate);
-                ShowMatrix(rotate);
+                matrixRotate90(colors, rotate);
+                showMatrix(rotate);
                 break;
             case 2:
-                MatrixRotate180(colors, rotate);
-                ShowMatrix(rotate);
+                matrixRotate180(colors, rotate);
+                showMatrix(rotate);
                 break;
             case 3:
-                MatrixRotate270(colors, rotate);
-                ShowMatrix(rotate);
+                matrixRotate270(colors, rotate);
+                showMatrix(rotate);
                 break;
             default:
                 System.out.println("Такой команды нет");
         }
     }
 
-    private static void ShowMenu() {
+    private static void showMenu() {
         System.out.println("Введите номер команды:\n" +
                 "1. Поворот на 90 градусов\n" +
                 "2. Поворот на 180 градусов\n" +
                 "3. Поворот на 270 градусов\n");
     }
 
-    public static void CreateMatrix(int[][] colors) {
+    public static void createMatrix(int[][] colors) {
         Random random = new Random();
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
@@ -47,8 +47,7 @@ public class MatrixRotate {
         }
     }
 
-    public static void ShowMatrix(int[][] colors) {
-
+    public static void showMatrix(int[][] colors) {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 System.out.format("%4d", colors[i][j]);
@@ -58,8 +57,7 @@ public class MatrixRotate {
         System.out.println();
     }
 
-    public static int[][] MatrixRotate90(int[][] colors, int[][] rotate) {
-        //int[][] rotated90 = new int[SIZE][SIZE];
+    public static int[][] matrixRotate90(int[][] colors, int[][] rotate) {
         for (int x = 0; x < SIZE; x++) {
             for (int y = 0; y < SIZE; y++) {
                 rotate[x][y] = colors[SIZE - 1 - y][x];
@@ -68,8 +66,7 @@ public class MatrixRotate {
         return rotate;
     }
 
-    public static int[][] MatrixRotate180(int[][] colors, int[][] rotate) {
-        //int[][] rotated180 = new int[SIZE][SIZE];
+    public static int[][] matrixRotate180(int[][] colors, int[][] rotate) {
         for (int x = 0; x < SIZE; x++) {
             for (int y = 0; y < SIZE; y++) {
                 rotate[x][y] = colors[SIZE - 1 - x][SIZE - 1 - y];
@@ -78,8 +75,7 @@ public class MatrixRotate {
         return rotate;
     }
 
-    public static int[][] MatrixRotate270(int[][] colors, int[][] rotate) {
-        //int[][] rotated270 = new int[SIZE][SIZE];
+    public static int[][] matrixRotate270(int[][] colors, int[][] rotate) {
         for (int x = 0; x < SIZE; x++) {
             for (int y = 0; y < SIZE; y++) {
                 rotate[x][y] = colors[y][SIZE - 1 - x];
